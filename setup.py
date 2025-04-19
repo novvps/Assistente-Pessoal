e44e2fa1aee779b74c8f278ade5f244a0027cadb
Apolo <paolacrisnvs@gmail.com>
2025-04-19 11:56:37 -0300

    deletado

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="DivoTech",
-    version="1.0",
-    description="Divo Tech é o meu assistente pessoal.",
-    executables=[Executable("assistente.py")]
-)
